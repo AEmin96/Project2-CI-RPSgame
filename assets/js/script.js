0ca31dbae8013b18c409abfcab3c941d0f1382d1
AEmin96 <maxxseed00@yahoo.com>
2023-04-02 23:01:43 +0000

    Release v-1.2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+let pcResult = document.getElementById('pcchose');
+
 let userScore = document.getElementById('youscore');
 let pcScore = document.getElementById('pcscore');
 
@@ -9,20 +11,26 @@ rockBtn.addEventListener('click', function() { playGame(0); });
 paperBtn.addEventListener('click', function() { playGame(1); });
 scissorsBtn.addEventListener('click', function() { playGame(2); });
 
+
 function playGame(userChoice) {
-  let choices = ['Rock', 'Paper', 'Scissors'];
+  
+  let choices = ['<i class="fa-solid fa-hand-back-fist"></i>', '<i class="fa-solid fa-hand"></i>', '<i class="fa-sharp fa-solid fa-hand-scissors"></i>'];
+ 
   let pcChoice = Math.floor(Math.random() * 3);
  
   if (userChoice === 0 && pcChoice === 2 || 
       userChoice === 1 && pcChoice === 0 || 
       userChoice === 2 && pcChoice === 1) {
     userScore.textContent = parseInt(userScore.textContent) + 1;
-    alert(`PC Chose : ${choices[pcChoice]} , You win!`);
+    pcResult.innerHTML = `PC Chose : ${choices[pcChoice]} <br> <em> You win! </em> `
+    //alert(`PC Chose : ${choices[pcChoice]} , You win!`);
   } else if (userChoice === pcChoice) {
-    alert(`PC Chose : ${choices[pcChoice]} , Draw Game!`);
+    pcResult.innerHTML = `PC Chose : ${choices[pcChoice]} <br> <em> Draw! </em> `
+    //alert(`PC Chose : ${choices[pcChoice]} , Draw Game!`);
   } else {
+    pcResult.innerHTML = `PC Chose : ${choices[pcChoice]} <br> <em> PC Wins! <em>`
     pcScore.textContent = parseInt(pcScore.textContent) + 1;
-    alert(`PC Chose : ${choices[pcChoice]} , PC wins!`);
+    //alert(`PC Chose : ${choices[pcChoice]} , PC wins!`);
   }
 }
 
