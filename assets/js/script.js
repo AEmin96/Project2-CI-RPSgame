ae71c3fa6e0b4169216538fabb736f6f3365a8d7
AEmin96 <maxxseed00@yahoo.com>
2023-04-02 23:25:12 +0000

    deploy-rel-v.1

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,13 +22,13 @@ function playGame(userChoice) {
       userChoice === 1 && pcChoice === 0 || 
       userChoice === 2 && pcChoice === 1) {
     userScore.textContent = parseInt(userScore.textContent) + 1;
-    pcResult.innerHTML = `PC Chose : ${choices[pcChoice]} <br> <em> You win! </em> `
+    pcResult.innerHTML = `PC Chose : ${choices[pcChoice]} <br> <em> You win! </em> `;
     //alert(`PC Chose : ${choices[pcChoice]} , You win!`);
   } else if (userChoice === pcChoice) {
-    pcResult.innerHTML = `PC Chose : ${choices[pcChoice]} <br> <em> Draw! </em> `
+    pcResult.innerHTML = `PC Chose : ${choices[pcChoice]} <br> <em> Draw! </em> `;
     //alert(`PC Chose : ${choices[pcChoice]} , Draw Game!`);
   } else {
-    pcResult.innerHTML = `PC Chose : ${choices[pcChoice]} <br> <em> PC Wins! <em>`
+    pcResult.innerHTML = `PC Chose : ${choices[pcChoice]} <br> <em> PC Wins! <em>`;
     pcScore.textContent = parseInt(pcScore.textContent) + 1;
     //alert(`PC Chose : ${choices[pcChoice]} , PC wins!`);
   }
